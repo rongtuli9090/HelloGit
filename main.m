46bf67722699de874eafbee92130c12df15f9019
rongtuli9090 <rongtuli9090@gmail.com>
2015-05-02 02:35:27 -0400

    added slog message

--- main.m
+++ main.m
@@ -11,6 +11,8 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        NSLog(@"Go Go");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
